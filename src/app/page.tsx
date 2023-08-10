e8b290328e9d35300112ba5ef1241a9e5769ebe6
陈金辉 <adam@chenjinhuideMacBook-Pro.local>
2023-08-10 23:37:32 +0800

    feat:home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
-import Image from "next/image";
+"use client";
+import { Card, Row, Col, Divider, Typography } from "antd";
+import Link from "next/link";
+const { Title, Paragraph } = Typography;
 
 export default function Home() {
-  return <div>next.js</div>;
+  return (
+    <div style={{ width: 1200, margin: "50px auto" }}>
+      <Typography>
+        <Title>项目介绍</Title>
+        <Paragraph>
+          下班后,周末时,喜欢学一些新的技术,
+          Next.js13.4是一个大的更新,自我认为next.js在Vercel公司带领下会成为前端技术框架的主流.学习思考框架中一些主流的技术
+        </Paragraph>
+        <Paragraph>
+          <ul>
+            <li>app路由器</li>
+            <li>SSR/CSR</li>
+            <li>嵌套管线和布局</li>
+            <li>简化的数据获取</li>
+            <li>流式处理和暂停</li>
+            <li>内置SEO支持</li>
+            <li>Turbopack</li>
+            <li>服务器操作...</li>
+          </ul>
+        </Paragraph>
+        <Title>项目</Title>
+        <div style={{ display: "flex", gap: 50 }}>
+          <Card title="新闻前台" extra={<a href="/news">地址</a>} style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          <Card title="新闻管理后台" extra={<a href="/admin/login">地址</a>} style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
+      </Typography>
+    </div>
+  );
 }
